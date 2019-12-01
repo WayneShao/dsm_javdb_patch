cbe48953fc81613bda48b3b57a1ef1b0c2114529
temp <245646299@qq.com>
2019-12-01 16:32:59 +0800

    fix download url

--- dsm_javdb_patch.sh
+++ dsm_javdb_patch.sh
@@ -24,8 +24,8 @@ function install()
 	mv /var/packages/VideoStation/target/plugins/syno_synovideodb/search.php /var/packages/VideoStation/target/plugins/syno_synovideodb/search.php.javback
 	mv /var/packages/VideoStation/target/ui/videostation2.js /var/packages/VideoStation/target/ui/videostation2.js.javback
 
-	wget --no-check-certificate https://gitee.com/challengerV/dsm_javdb_path/raw/master/dsm_javdb_path.tar -O dsm_javdb_path.tar;
-	tar -xvf dsm_javdb_path.tar
+	wget --no-check-certificate https://gitee.com/challengerV/dsm_javdb_patch/raw/master/dsm_javdb_patch.tar -O dsm_javdb_patch.tar;
+	tar -xvf dsm_javdb_patch.tar
 
 	\cp -rfa ./syno_themoviedb /var/packages/VideoStation/target/plugins/;
 	\cp -rfa ./syno_synovideodb /var/packages/VideoStation/target/plugins/;
