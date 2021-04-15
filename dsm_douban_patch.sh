96df05adf8721c154345aa48c6666792df74019a
siryle1213 <siryle1213@126.com>
2021-04-15 13:34:11 +0800

    update dsm_douban_patch.sh.

--- dsm_douban_patch.sh
+++ dsm_douban_patch.sh
@@ -18,10 +18,6 @@ function install()
 {
     cd /tmp/;
 
-    mv /var/packages/VideoStation/target/plugins/syno_themoviedb/search.php /var/packages/VideoStation/target/plugins/syno_themoviedb/search.php.doubanback
-    mv /var/packages/VideoStation/target/plugins/syno_themoviedb_tv/search.php /var/packages/VideoStation/target/plugins/syno_themoviedb_tv/search.php.doubanback
-
-
     wget --no-check-certificate https://gitee.com/siryle1213/dsm_javdb_patch/raw/master/dsm_douban_patch.tar -O dsm_douban_patch.tar;
     tar -xvf dsm_douban_patch.tar
 
@@ -42,45 +38,9 @@ function install()
     echo "恭喜, DS Video Douban 搜刮器补丁 $vVersion 安装完成！";
     echo '==========================================================================';
 }
-function uninstall()
-{   
-    rm /var/packages/VideoStation/target/plugins/syno_file_assets/douban.php
-    
-    mv -f /var/packages/VideoStation/target/plugins/syno_themoviedb/search.php.doubanback /var/packages/VideoStation/target/plugins/syno_themoviedb/search.php
-    mv -f /var/packages/VideoStation/target/plugins/syno_themoviedb_tv/search.php.doubanback /var/packages/VideoStation/target/plugins/syno_themoviedb_tv/search.php
-
-    
-    chmod 0755 /var/packages/VideoStation/target/plugins/syno_themoviedb/search.php
-    chmod 0755 /var/packages/VideoStation/target/plugins/syno_themoviedb_tv/search.php
-
-    chown VideoStation:VideoStation /var/packages/VideoStation/target/plugins/syno_themoviedb/search.php
-    chown VideoStation:VideoStation /var/packages/VideoStation/target/plugins/syno_themoviedb_tv/search.php
-
-    
-    
-    echo 'DS Video Douban 搜刮器补丁 卸载完成！ QQ 群:160128165';
-    echo '==========================================================================';
-}
-
 # SHELL     ******************************************************************
 if [ "$vAction" == 'install' ]; then
-    if [ ! -f "/var/packages/VideoStation/target/plugins/syno_themoviedb/search.php.doubanback" ]; then
-        install;
-    else
-        echo '你已经安装过 DS Video JavBus 或 JavDB 或 JavBus+JavDB+Douban 搜刮器补丁. QQ 群:160128165';
-        echo '==========================================================================';
-        rm -rf dsm_douban_patch.sh
-        exit 1;
-    fi;
-elif [ "$vAction" == 'uninstall' ]; then
-    if [ ! -f "/var/packages/VideoStation/target/plugins/syno_themoviedb/search.php.doubanback" ]; then
-        echo '你还没安装过 DS Video Douban 搜刮器补丁，无需卸载. QQ 群:160128165';
-        echo '==========================================================================';
-        rm -rf dsm_douban_patch.sh
-        exit 1;
-    else
-        uninstall;
-    fi;
+    install;
 else
     echo '错误的命令';
     echo '==========================================================================';
